3ba5c3bfb889ff2093a72cf84958617040782937
jedens5000 <70732210+jedens5000@users.noreply.github.com>
2022-08-31 19:40:15 -0400

    Keyboard and click events working

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
-for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+const drums = document.querySelectorAll(".drum");
 
-  function handleClick() {
-    // console.log("click handler " + i);
-    // console.log(this.innerHTML);
-    // function hitDrum() {
+for (let i = 0; i < drums.length; i++) {
+  drums[i].addEventListener("click", clickDrum);
+  console.log(i);
+
+  function clickDrum() {
     if (i === 0) {
-      let audio = new Audio("./sounds/tom-1.mp3");
-      audio.play();
+      let tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
     } else if (i === 1) {
-      let audio = new Audio("./sounds/tom-2.mp3");
-      audio.play();
+      let tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
     } else if (i === 2) {
-      let audio = new Audio("./sounds/tom-3.mp3");
-      audio.play();
+      let tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
     } else if (i === 3) {
-      let audio = new Audio("./sounds/tom-4.mp3");
-      audio.play();
+      let tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
     } else if (i === 4) {
-      let audio = new Audio("./sounds/snare.mp3");
-      audio.play();
+      let snare = new Audio("./sounds/snare.mp3");
+      snare.play();
     } else if (i === 5) {
-      let audio = new Audio("./sounds/crash.mp3");
-      audio.play();
+      let crash = new Audio("./sounds/crash.mp3");
+      crash.play();
     } else {
-      let audio = new Audio("./sounds/kick-bass.mp3");
-      audio.play();
+      let bass = new Audio("./sounds/kick-bass.mp3");
+      bass.play();
     }
-    // audio.play();
-    // }
-
-    // hitDrum();
   }
 }
 
-// audio.play();
+document.addEventListener("keydown", typeDrum);
+
+function typeDrum(e) {
+  console.log(e.keyCode);
+  if (e.keyCode === 87) {
+    let tom1 = new Audio("./sounds/tom-1.mp3");
+    tom1.play();
+  } else if (e.keyCode === 65) {
+    let tom2 = new Audio("./sounds/tom-2.mp3");
+    tom2.play();
+  } else if (e.keyCode === 83) {
+    let tom3 = new Audio("./sounds/tom-3.mp3");
+    tom3.play();
+  } else if (e.keyCode === 68) {
+    let tom4 = new Audio("./sounds/tom-4.mp3");
+    tom4.play();
+  } else if (e.keyCode === 74) {
+    let snare = new Audio("./sounds/snare.mp3");
+    snare.play();
+  } else if (e.keyCode === 75) {
+    let crash = new Audio("./sounds/crash.mp3");
+    crash.play();
+  } else if (e.keyCode === 76) {
+    let bass = new Audio("./sounds/kick-bass.mp3");
+    bass.play();
+  }
+}
